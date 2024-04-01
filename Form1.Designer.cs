0f09243d6d0cfca1e17cfe617c5472b33a1859b5
zhaojinweiOWO <vincentcv94@gmail.com>
2024-04-01 22:18:01 +0800

    4/1修改

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LengthCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCM = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtKM = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtFt = new System.Windows.Forms.TextBox();
+            this.txtYard = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Location = new System.Drawing.Point(82, 60);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(150, 46);
+            this.btnAllClear.TabIndex = 0;
+            this.btnAllClear.Text = "清除";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "公分";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "公尺";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(609, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "公里";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "英吋";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(346, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "英呎";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(609, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "碼";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(85, 410);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "說明文字";
+            // 
+            // txtCM
+            // 
+            this.txtCM.Location = new System.Drawing.Point(82, 190);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(160, 29);
+            this.txtCM.TabIndex = 8;
+            this.txtCM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCM_KeyUp);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(349, 190);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(160, 29);
+            this.txtM.TabIndex = 9;
+            this.txtM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtM_KeyUp);
+            // 
+            // txtKM
+            // 
+            this.txtKM.Location = new System.Drawing.Point(612, 190);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(160, 29);
+            this.txtKM.TabIndex = 10;
+            this.txtKM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKM_KeyUp);
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(88, 331);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(160, 29);
+            this.txtIn.TabIndex = 11;
+            this.txtIn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtIn_KeyUp);
+            // 
+            // txtFt
+            // 
+            this.txtFt.Location = new System.Drawing.Point(349, 331);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(160, 29);
+            this.txtFt.TabIndex = 12;
+            this.txtFt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFt_KeyUp);
+            // 
+            // txtYard
+            // 
+            this.txtYard.Location = new System.Drawing.Point(612, 331);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(160, 29);
+            this.txtYard.TabIndex = 13;
+            this.txtYard.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtYard_KeyUp);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(88, 442);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(684, 29);
+            this.txtInfo.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 557);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtCM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAllClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCM;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtKM;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtFt;
+        private System.Windows.Forms.TextBox txtYard;
+        private System.Windows.Forms.TextBox txtInfo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LengthCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strInput; 
+        double douOutput; 
+
+        private void txtCM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtCM.Text;
+
+            
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(0, douOutput);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
+                txtCM.Text = "";
+            }
+        }
+
+        private void txtM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtM.Text;
+
+          
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(1, douOutput * 100);
+            }
+            else
+            {
+              
+                txtInfo.Text = "請輸入數字";
+                txtM.Text = "";
+            }
+        }
+
+        private void txtKM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtKM.Text; 
+
+            
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(2, douOutput * 10000);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
+                txtKM.Text = "";
+            }
+        }
+
+        private void txtIn_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtIn.Text;
+
+           
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(3, douOutput * 2.54);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
+                txtIn.Text = "";
+            }
+        }
+
+        private void txtFt_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtFt.Text; 
+
+          
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(4, douOutput * 30.48);
+            }
+            else
+            {
+                
+                txtInfo.Text = "請輸入數字";
+                txtFt.Text = "";
+            }
+        }
+
+        private void txtYard_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtYard.Text;
+
+           
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(5, douOutput * 91.44);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
+                txtYard.Text = "";
+            }
+        }
+
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            txtCM.Text = "";
+            txtM.Text = "";
+            txtKM.Text = "";
+            txtIn.Text = "";
+            txtFt.Text = "";
+            txtYard.Text = "";
+            txtInfo.Text = "";
+        }
+
+        
+        private void caculateAnswer(int _kind, double _value)
+        {
+            if (_kind != 0)
+                txtCM.Text = string.Format("{0:0.##########}", _value);
+            if (_kind != 1)
+                txtM.Text = string.Format("{0:0.##########}", _value / 100);
+            if (_kind != 2)
+                txtKM.Text = string.Format("{0:0.##########}", _value / 100000);
+            if (_kind != 3)
+                txtIn.Text = string.Format("{0:0.##########}", _value / 2.54);
+            if (_kind != 4)
+                txtFt.Text = string.Format("{0:0.##########}", _value / 30.48);
+            if (_kind != 5)
+                txtYard.Text = string.Format("{0:0.##########}", _value / 91.44);
+        }
+    }
+}
